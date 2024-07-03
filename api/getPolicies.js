e53251e156340a161e2d72a493f1c4cf39d50c12
Jeremy Kim <jeremy@namawell.com>
2024-07-02 17:39:15 -0700

    gql request

--- api/getPolicies.js
+++ api/getPolicies.js
@@ -16,44 +16,43 @@ export default async function handler(req, res) {
   const query = `
     {
       shop {
-        policies {
+        shopPolicies {
+          type
           body
-          title
           url
         }
       }
     }
   `;
 
-//   try {
-//     const response = await axios.post(
-//       `https://${SHOPIFY_STORE}.myshopify.com/admin/api/2023-01/graphql.json`,
-//       { query },
-//       {
-//         headers: {
-//           'X-Shopify-Access-Token': SHOPIFY_ADMIN_API_TOKEN,
-//           'Content-Type': 'application/json',
-//         },
-//       }
-//     );
-
-//     res.status(200).json(response.data);
-//   } catch (error) {
-//     console.log(`api pw: ${SHOPIFY_ADMIN_API_TOKEN}, store: ${SHOPIFY_STORE}`);
-//     console.error("Error making request:", error.response ? error.response.data : error);
-//     res.status(500).json({ error: error.message });
-//   }
-
   try {
-    const response = await axios.get(`https://${SHOPIFY_STORE}.myshopify.com/admin/api/2023-10/policies.json`, {
+    const response = await axios.post(
+      `https://${SHOPIFY_STORE}.myshopify.com/admin/api/2023-10/graphql.json`,
+      { query },
+      {
         headers: {
-            'X-Shopify-Access-Token': SHOPIFY_ADMIN_API_TOKEN,
-            'Content-Type': 'application/json',
+          'X-Shopify-Access-Token': SHOPIFY_ADMIN_API_TOKEN,
+          'Content-Type': 'application/json',
         },
-    })
-    res.status(200).json(response.data)
+      }
+    );
+
+    res.status(200).json(response.data);
   } catch (error) {
-    console.error("Error making request:", error.response ? error.response.data : error);
-    res.status(500).json({ error: error.message })
+    console.error("Error:", error.response ? error.response.data : error);
+    res.status(500).json({ error: error.message });
   }
+
+//   try {
+//     const response = await axios.get(`https://${SHOPIFY_STORE}.myshopify.com/admin/api/2023-10/policies.json`, {
+//         headers: {
+//             'X-Shopify-Access-Token': SHOPIFY_ADMIN_API_TOKEN,
+//             'Content-Type': 'application/json',
+//         },
+//     })
+//     res.status(200).json(response.data)
+//   } catch (error) {
+//     console.error("Error:", error.response ? error.response.data : error);
+//     res.status(500).json({ error: error.message })
+//   }
 }
